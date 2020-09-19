2fea0d1bb3aeb122ee36a88ca24f8e258a3686f0
embydextrous <arjitagarwal099@gmail.com>
2020-09-19 19:26:00 +0530

    Convert using zip

--- changeStringToNewCharacterSet.py
+++ changeStringToNewCharacterSet.py
@@ -1,8 +1,16 @@
 from string import maketrans
 
-def convert(s):
-    mapped = maketrans("qwertyuiopasdfghjklzxcvbnm", "abcdefghijklmnopqrstuvwxyz")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+def convert(s, characterSet):
+    mapped = maketrans(characterSet, alphabet)
     return s.translate(mapped)
 
+def convertUsingZip(s, characterSet):
+    a = dict(zip(characterSet, alphabet))
+    s = [a[i] for i in s]
+    return "".join(s)
+
+newCharacterSet = "qwertyuiopasdfghjklzxcvbnm"
 s = "utta"
-print convert(s)
+print convert(s, newCharacterSet)
+print convertUsingZip(s, newCharacterSet)
