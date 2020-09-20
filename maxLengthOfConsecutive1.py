295a4abf49fa85cdc47f2a3fbfc065bcd8ffe576
embydextrous <arjitagarwal099@gmail.com>
2020-09-20 16:41:09 +0530

    Maximum length of consecutive 1's in a binary string raw implementation

--- maxLengthOfConsecutive1.py
+++ maxLengthOfConsecutive1.py
@@ -2,5 +2,17 @@ def maxLengthOf1InBinaryString(s):
     a = s.split("0")
     return max(map(len, a))
 
+def maxLengthOf1InBinaryStringRaw(s):
+    maxLength = 0
+    count = 0
+    for i in range(len(s)):
+        if s[i] == '1':
+            count += 1
+        else:
+            if count > maxLength:
+                maxLength = count
+            count = 0
+    return maxLength
+
 s = "101110101011110110110101010101000"
-print maxLengthOf1InBinaryString(s)
+print maxLengthOf1InBinaryStringRaw(s)
